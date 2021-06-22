792427d8b7f8d1548151ef887169c55d7fd76262
André Luís Moreira <andre.moreira12@etec.sp.gov.br>
2021-06-22 09:14:15 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsService } from './restaurants/restaurants.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [RestaurantsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,26 @@
+import { Restaurant } from './restaurant/restaurant.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MEAT_API } from '../app.api';
+import { map } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantsService {
+
+  constructor(private http: HttpClient,
+    private toastr: ToastrService) { }
+
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`).pipe(
+      map(restaurants => restaurants)
+    );
+  }
+
+  exibirMensagem(titulo: string, mensagem: string, tipo: string):void{
+
+  }
+}
